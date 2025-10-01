6f5130df871b725a34128ade5fb773673e855961
KietNT <113796420+TanNhatCMS@users.noreply.github.com>
2025-10-01 22:27:03 +0700

    Enhance publish configuration with versioning and beta release options

--- configs/publish.ts
+++ configs/publish.ts
@@ -1,10 +1,17 @@
 import type { GithubOptions } from 'builder-util-runtime'
+import pkg from '../package.json'
 
-const conf: GithubOptions = {
+const isBeta = false // Đặt thành true khi phát hành bản beta
+const allowBetaUpdate = false // Đặt thành true để cho phép cập nhật beta
+
+const conf: GithubOptions & { version: string; isBeta: boolean; allowBetaUpdate: boolean } = {
   provider: 'github',
   owner: 'TanNhatCMS',
   repo: 'FlyEnv',
-  releaseType: 'release'
+  releaseType: isBeta || allowBetaUpdate ? 'prerelease' : 'release',
+  version: pkg.version,
+  isBeta,
+  allowBetaUpdate
 }
 
 export default conf
